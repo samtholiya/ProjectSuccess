dde9f1816c57192b6c32dd2f40018425972824a1
samtholiya <tholiya.sam@outlook.in>
2016-08-31 23:42:20 +0530

    Add project files.

---
+++ ProjectSuccess/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectSuccess.MvcApplication" Language="C#" %>
---
+++ ProjectSuccess/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectSuccess.Startup))]
+namespace ProjectSuccess
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
